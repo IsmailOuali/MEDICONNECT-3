47389f18840bf76562b55b7c3a3de2ce46eb4521
unknown <ismail.ouali@ump.ac.ma>
2024-02-19 16:57:51 +0100

    Initializing the project

---
+++ project/database/migrations/2024_02_18_193707_create_appointments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->enum('heure',["sh1","sh2","sh3","sh4"]);
+            $table->foreignId('patient_id')->constrained('patients');
+            $table->foreignId('doctor_id')->constrained('doctors');
+            $table->boolean('consulted')->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
